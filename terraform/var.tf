40002c6df94e5b979de2ff8adc357bbfd4d11de1
anksgoel99 <35847759+anksgoel99@users.noreply.github.com>
2021-03-24 21:20:33 +0530

    Terraform script to create multiple azure instance

---
+++ terraform/var.tf
@@ -0,0 +1,20 @@
+variable "location" {
+  default = "eastus"
+}
+
+variable "prefix" {
+  default = "ank-tf"
+}
+
+variable "tags" {
+  default = "ank-softnas-perf"
+}
+
+variable "win_vm_size" {
+  default = "Standard_DS14_v2"
+}
+
+variable "vm_count" {
+  default = "34"
+  
+} 
